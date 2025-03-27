c0f7ed9e8ccef00c227292d6c94a7308d2fc74d9
stephen0804 <stephenevans0804@hotmail.com>
2025-03-27 19:12:20 +1030

    update main.py with func to print text file to console

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        book_contents = f.read()
+    return(book_contents)
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
