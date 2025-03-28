d9e215dbc36f6101d4182c0c20b57dd88bd90eff
stephen0804 <stephenevans0804@hotmail.com>
2025-03-28 18:36:36 +1030

    update main.py with function to return number of words in string

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+book = "books/frankenstein.txt"
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         book_contents = f.read()
     return(book_contents)
 
+def get_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text(book)
+    book_words = get_words(book_text)
+    print(f"{book_words} words found in the document")
 
 main()
