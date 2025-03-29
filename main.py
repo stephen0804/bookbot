1d090246bf18fed0b1c29437c49a7f4c387f46ed
stephen0804 <stephenevans0804@hotmail.com>
2025-03-29 18:10:03 +1030

    add stats.py update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from stats import get_words
+
 book = "books/frankenstein.txt"
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         book_contents = f.read()
     return(book_contents)
-
-def get_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book_text = get_book_text(book)
---
+++ stats.py
@@ -0,0 +1,4 @@
+
+def get_words(text):
+    words = text.split()
+    return len(words)
